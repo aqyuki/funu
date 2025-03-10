b0e1d23f4c415fd824388fd4847bf7735fe8a5e8
aqyuki <aqyuki@aqyuki.dev>
2025-03-10 16:54:11 +0900

    feat: キャラクターを画面の中心に描画するように変更

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct WindowState {
+    pub width: u32,
+    pub height: u32,
+}
+
+impl WindowState {
+    pub fn new(width: u32, height: u32) -> Self {
+        WindowState {
+            width: width,
+            height: height,
+        }
+    }
+}
+
+const CHARACTER_WIDTH: u32 = 100;
+const CHARACTER_HEIGHT: u32 = 100;
+
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -7,6 +24,9 @@ fn main() {
         .vulkan()
         .build()
         .unwrap();
+
+    let window_size = window.size();
+    let window_state = WindowState::new(window_size.0, window_size.1);
 
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -37,7 +57,12 @@ fn main() {
         // キャラクター (を模した長方形)
         canvas.set_draw_color(sdl2::pixels::Color::BLACK);
         canvas
-            .fill_rect(sdl2::rect::Rect::new(100, 100, 100, 100))
+            .fill_rect(sdl2::rect::Rect::new(
+                (window_state.width as i32 / 2) - (CHARACTER_WIDTH as i32 / 2),
+                (window_state.height as i32 / 2) - (CHARACTER_HEIGHT as i32 / 2),
+                CHARACTER_WIDTH,
+                CHARACTER_HEIGHT,
+            ))
             .unwrap();
 
         canvas.present();
