570b57a88051197fa5ff139575b10e2502f3772c
aqyuki <aqyuki@aqyuki.dev>
2025-03-10 16:21:45 +0900

    feat: 背景を白で描画するようにした

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let _window = video_subsystem
+    let window = video_subsystem
         .window("Hello SLD2", 800, 500)
         .position_centered()
         .vulkan()
         .build()
         .unwrap();
 
+    let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    // 初期描画
+    canvas.set_draw_color(sdl2::pixels::Color::WHITE);
+    canvas.clear();
+    canvas.present();
 
     'running: loop {
         for event in event_pump.poll_iter() {
