7ad4ac0867893253cdcd24c5625f4fd6ed4444e3
aqyuki <aqyuki@aqyuki.dev>
2025-03-10 15:36:07 +0900

    feat: windowが一瞬表示されるようになった

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let _window = video_subsystem
+        .window("Hello SLD2", 800, 500)
+        .position_centered()
+        .vulkan()
+        .build()
+        .unwrap();
 }
